37d5a1e7bbce59cfa8af4a43b4a00f84c33505fa
Mileta Cekovic <mileta.cekovic@gtechg2.com>
2012-08-20 20:36:59 +0200

    Time in TracingCurrencyRateProviderProxy

--- core/src/main/java/org/strangeforest/currencywatch/core/TracingCurrencyRateProviderProxy.java
+++ core/src/main/java/org/strangeforest/currencywatch/core/TracingCurrencyRateProviderProxy.java
@@ -4,6 +4,8 @@ import java.util.*;
 
 import org.slf4j.*;
 
+import com.finsoft.util.*;
+
 import static java.lang.String.*;
 
 public class TracingCurrencyRateProviderProxy extends BaseCurrencyRateProvider {
@@ -30,20 +32,28 @@ public class TracingCurrencyRateProviderProxy extends BaseCurrencyRateProvider {
 	}
 
 	@Override public RateValue getRate(String baseCurrency, String currency, Date date) {
-		if (logger.isTraceEnabled())
+		boolean traceEnabled = logger.isTraceEnabled();
+		StopWatch watch = null;
+		if (traceEnabled) {
 			logger.trace(format("getRate(baseCurrency=%1$s, currency=%2$s, date=%3$td-%3$tm-%3$tY)", baseCurrency, currency, date));
+			watch = new StopWatch();
+		}
 		RateValue rate = provider.getRate(baseCurrency, currency, date);
-		if (logger.isTraceEnabled())
-			logger.trace(format("~getRate: %1$s", rate));
+		if (traceEnabled)
+			logger.trace(format("~getRate=%1$s [%2$s]", rate, watch.time()));
 		return rate;
 	}
 
 	@Override public Map<Date, RateValue> getRates(String baseCurrency, String currency, Collection<Date> dates) {
-		if (logger.isTraceEnabled())
+		boolean traceEnabled = logger.isTraceEnabled();
+		StopWatch watch = null;
+		if (traceEnabled) {
 			logger.trace(format("getRates(baseCurrency=%1$s, currency=%2$s, dates=Date[%3$d])", baseCurrency, currency, dates.size()));
+			watch = new StopWatch();
+		}
 		Map<Date, RateValue> rates = provider.getRates(baseCurrency, currency, dates);
-		if (logger.isTraceEnabled())
-			logger.trace(format("~getRates: Map<Date, RateValue>[%1$d]", rates.size()));
+		if (traceEnabled)
+			logger.trace(format("~getRates=Map<Date, RateValue>[%1$d] [%2$s]", rates.size(), watch.time()));
 		return rates;
 	}
 }
